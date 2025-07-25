1034f8350fe9fc1115b2785cdd7c59dce215a403
Muhammad Safaat <paatlupi01@gmail.com>
2025-07-25 16:06:53 +0700

    change database,and create sequelize

--- database/index.js
+++ database/index.js
@@ -1,20 +1,44 @@
-import mysql from "mysql2";
+// import mysql from "mysql2";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DBNAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+// pool.getConnection((err, conn) => {
+//     if (err) console.log(err)
+//     console.log("Connected Database successfully")
+// })
+
+// export default pool.promise();
+
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
 
-pool.getConnection((err, conn) => {
-    if (err) console.log(err)
-    console.log("Connected Database successfully")
-})
+export const sequelize = new Sequelize(
+    process.env.DB_DBNAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false
+    }
+)
 
-export default pool.promise();
+// 🔍 Cek koneksi database
+sequelize.authenticate()
+    .then(() => {
+        console.log("✅ Connected to MySQL database via Sequelize");
+    })
+    .catch((err) => {
+        console.error("❌ Unable to connect to the database:", err);
+    });
