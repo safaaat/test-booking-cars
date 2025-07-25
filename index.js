74bf8c71e457faa806ab351650f4165d2b599a5d
Muhammad Safaat <paatlupi01@gmail.com>
2025-07-25 15:28:11 +0700

    create database

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import expres from "express";
 import dotenv from "dotenv";
+import pool from "./database/index.js";
 
 dotenv.config();
 const app = expres();
 
 const PORT = process.env.PORT || 5000;
+
+// Test koneksi database
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("Database connection failed:", err);
+    } else {
+        console.log("✅ Connected to MySQL database");
+        connection.release(); // penting: lepas koneksi setelah dipakai
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Run Server ${PORT}`);
